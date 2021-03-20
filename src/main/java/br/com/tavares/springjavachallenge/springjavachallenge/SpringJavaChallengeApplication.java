8267700d9da213b4a8a457a8b64f0a4d3f825dbf
alexandre <altavares@grupodimed.com.br>
2021-03-19 22:19:48 -0300

    Iniciado projeto, criado modulos do projeto e versionamento

---
+++ src/main/java/br/com/tavares/springjavachallenge/springjavachallenge/SpringJavaChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.tavares.springjavachallenge.springjavachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tavares/springjavachallenge/springjavachallenge/SpringJavaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tavares.springjavachallenge.springjavachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
